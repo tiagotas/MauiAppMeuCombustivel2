572ebb7278eed25de5675fcf6d4a3c5e49ed5d48
Usuario <Usuario@CPD02-M00>
2024-02-26 15:09:33 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombustivel2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppMeuCombustivel2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
